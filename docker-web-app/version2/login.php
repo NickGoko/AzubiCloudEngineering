021a8e108b49ef5189e7a6d7a8ab5257a01ed586
NickGoko <nickmainagoko@gmail.com>
2024-02-24 09:06:54 +0300

    update README

--- docker-web-app/version2/login.php
+++ docker-web-app/version2/login.php
@@ -4,7 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    // Perform validation or processing here (e.g., check credentials against a database)
+
     // For demonstration purposes, we'll simply print the received data
     echo "Received username: " . $username . "<br>";
     echo "Received password: " . $password . "<br>";

a7dd8c730f145e7921ce3fae085e502108e20b38
Nick Goko <61675328+NickGoko@users.noreply.github.com>
2024-03-07 23:08:39 +0300

    fixing version2

--- docker-web-app/version2/login.php
+++ docker-web-app/version2/login.php
@@ -1,13 +1,14 @@
+<!-- version2/login.php -->
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve username and password from the form
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+$correct_username = "Beanstalk";
+$correct_password = "@dewrongs1d3";
 
+$entered_username = $_POST["username"];
+$entered_password = $_POST["password"];
 
-    // For demonstration purposes, we'll simply print the received data
-    echo "Received username: " . $username . "<br>";
-    echo "Received password: " . $password . "<br>";
+if ($entered_username == $correct_username && $entered_password == $correct_password) {
+    echo "Welcome! This is an admin";
+} else {
+    echo "Invalid username or password";
 }
 ?>
-
